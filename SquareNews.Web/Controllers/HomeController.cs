9be3d18dc7b131d73e97f4ee472d0111a5f03512
Anto <amervyn14@gmail.com>
2018-08-06 20:06:25 +0100

    web project started and returning article list

--- SquareNews.Web/Controllers/HomeController.cs
+++ SquareNews.Web/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SquareNews.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +13,37 @@ namespace SquareNews.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //Hosted web API REST Service base url  
+        string Baseurl = "http://192.168.95.1:5555/";
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var result = new ArticleResult();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("http://amervyn.duckdns.org/api/Articles/?fromDate=2018-08-06T13:00:00&pageSize=100");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    result = JsonConvert.DeserializeObject<ArticleResult>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(result);
+            }
         }
 
         public ActionResult About()
---
+++ SquareNews.Web/Models/ArticleResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SquareNews.Lib.Entities;
+
+namespace SquareNews.Web.Models
+{
+    public class ArticleResult
+    {
+        public int TotalResults { get; set; }
+        public List<NewsArticle> Articles { get; set; }
+    }
+}
